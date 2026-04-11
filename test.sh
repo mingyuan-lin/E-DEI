67e7c87b30ddfb0bb9e04ea6237dbb5c1a2afeb5
mylin <lin.mingyuan@outlook.com>
2026-04-11 19:55:36 +0800

    upload files

---
+++ test.sh
@@ -0,0 +1,3 @@
+python test.py --dataset_path ./datasets --dataset_name REDS --arch UNet_de_ca_denc_ddec --net_path ./weights/reds_edei_99_model.pth --exp_name reds_edei_99_model
+
+# python test.py --dataset_path ../../datasets --dataset_name RLIED --arch UNet_de_ca_denc_ddec --net_path ./weights/rlied_edei_99_model.pth --exp_name exp_1_0
